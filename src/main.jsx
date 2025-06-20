1af87fe1142248a902f43a44375743e605bbf8f0
Sonu Kumar Saw <162047577+sonukumarsaw12@users.noreply.github.com>
2025-06-20 15:19:57 +0530

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,34 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter } from 'react-router-dom'
+import { RouterProvider } from 'react-router'
+import CreateTrip from './create-trip'
+import Header from './components/custom/Header'
+import { Toaster } from './components/ui/sonner'
+import { Import } from 'lucide-react'
+import { GoogleOAuthProvider } from '@react-oauth/google'
+
+const router=createBrowserRouter([
+  {
+  path:'/',
+  element:<App/>
+  },
+  {
+    path:'/create-trip',
+    element:<CreateTrip/>
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_AUTH_CLIENT_ID}>
+    <Header/>
+    <Toaster  />
+    <RouterProvider router={router}/>
+    </GoogleOAuthProvider>
+    
+   
+  </StrictMode>,
+)
